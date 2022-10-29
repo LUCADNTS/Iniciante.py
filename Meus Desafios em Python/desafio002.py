fdb14fc9cbac8c13686d4160d915d144b11a3329
Lucas Brito <79122918+LUCADNTS@users.noreply.github.com>
2022-10-29 19:01:45 -0300

    att

--- Meus Desafios em Python/desafio002.py
+++ Meus Desafios em Python/desafio002.py
@@ -1,4 +1,4 @@
-'''faça um progrma que leia a mensagem de uma pessoa e mostre uma mensagem de boas vindas'''
+'''faça um programa que leia a mensagem de uma pessoa e mostre uma mensagem de boas vindas'''
 
 nome = input('digite seu nome: ')
 print('Prazer em te conhecer', nome)
--- Meus Desafios em Python/desafio003.py
+++ Meus Desafios em Python/desafio003.py
@@ -1,7 +1,19 @@
-''' crie um programa que leia dois numeros e mostre a soma entre eles'''
+''' 
+crie um programa que leia dois números e mostre a soma entre eles
 n = int(input('digite o primeiro numero: '))
 n2 = int(input('digite o segundo numero: '))
 
 s = (n + n2)
 
 print('a soma entre {} e {} é : {}'.format(n, n2, s))
+'''
+
+from asyncore import loop
+
+
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+
+s = a + b
+print('A soma entre {} e {} é: {}'.format(a, b , s))
+
